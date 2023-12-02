531444d33caa611f7ed1b01ad723b8f442f60a78
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-12-02 21:04:06 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgres://green_summer_user:1ftRAE0BG51IW3bTOBPfE5EcraaKfmx5@dpg-cl3016auuipc73846i10-a.oregon-postgres.render.com/green_summer'
+    connectionString: 'postgres://green_summer_user:1ftRAE0BG51IW3bTOBPfE5EcraaKfmx5@dpg-cl3016auuipc73846i10-a.oregon-postgres.render.com/green_summer?ssl=true'
 });
 
 module.exports = pool;
